1ee9e30c366716aa464a2106ae9faa2cc711d6da
ZakhiyaPC <pc.zakhiya@gmail.com>
2024-10-15 18:31:42 +0530

    incorporating my data

--- Using own data.R
+++ Using own data.R
@@ -1,1 +1,21 @@
 #INCORPORATING EXISTING DATA
+library(squidSim)
+data<-data_structure_csv
+head(data)
+squid_data <- simulate_population(
+  data_structure = data[,c("ACTOR","AGFRACTS")],
+  parameters = list(
+    ACTOR = list(
+      vcov = 0.2
+    ),
+    AGFRACTS = list(
+      vcov = 0.3
+    ),
+    residual = list(
+      vcov = 0.5
+    )
+  )
+)
+
+data <- get_population_data(squid_data)
+head(data)
